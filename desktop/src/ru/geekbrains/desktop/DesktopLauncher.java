bbe4db165b6c570667a26ddcb13d1fc9ef04d93e
alt277 <altima875@gmail.com>
2020-03-15 15:38:12 +0300

    make version 8

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -2,12 +2,15 @@ package ru.geekbrains.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.geekbrains.MyStarGame;
+
+import ru.geekbrains.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyStarGame(), config);
+		config.height = 600;
+		config.width = 450;
+		config.resizable = false;
+		new LwjglApplication(new StarGame(), config);
 	}
-	///
 }
